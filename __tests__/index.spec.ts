5b9a36008215d724fd4d5e5fc7d3045af4ea8dc3
Thiago Delgado Pinto <thiago-dp@bol.com.br>
2020-06-16 13:55:57 -0300

    Change config to use microbundle

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,4 +1,4 @@
-import { makeOptions, formatNumber, isPatternCorrect } from "../src/index";
+import { currencyFromLocale, extractCountryCode, formatNumber, isPatternCorrect, makeOptions } from '..';
 
 // import { NumberFormat, NumberFormatOptions } from '@formatjs/intl-numberformat';
 
@@ -130,6 +130,77 @@ describe( 'intl-number-helper', () => {
 		it( 'evaluates an empty string as correct', () => {
 			const r = isPatternCorrect( '' );
 			expect( r ).toBeTruthy();
+		} );
+
+	} );
+
+
+
+	describe( '#extractCountryCode', () => {
+
+		it( 'does not extract from an empty locale', () => {
+			const r = extractCountryCode( '' );
+			expect( r ).toBeNull();
+		} );
+
+		it( 'does not extract from an invalid locale', () => {
+			const r = extractCountryCode( 'foo bar' );
+			expect( r ).toBeNull();
+		} );
+
+		it( 'does not extract from a valid locale without country', () => {
+			const r = extractCountryCode( 'en' );
+			expect( r ).toBeNull();
+		} );
+
+		it( 'extracts from a valid locale with country code after dash', () => {
+			const r = extractCountryCode( 'en-US' );
+			expect( r ).toEqual( 'US' );
+		} );
+
+		it( 'extracts from a valid locale with country code in lowercase, after dash', () => {
+			const r = extractCountryCode( 'en-us' );
+			expect( r ).toEqual( 'US' );
+		} );
+
+		it( 'extracts from a valid locale with country code in lowercase, after underline', () => {
+			const r = extractCountryCode( 'en_us' );
+			expect( r ).toEqual( 'US' );
+		} );
+
+		it( 'extracts from a valid locale with country code in the third part', () => {
+			const r = extractCountryCode( 'zh-Hans-CN' );
+			expect( r ).toEqual( 'CN' );
+		} );
+
+	} );
+
+
+	describe( '#currencyFromLocale', () => {
+
+		it( 'does not extract it from an empty locale', () => {
+			const r = currencyFromLocale( '' );
+			expect( r ).toEqual( null );
+		} );
+
+		it( 'does not extract it from an invalid locale', () => {
+			const r = currencyFromLocale( 'WRONG' );
+			expect( r ).toEqual( null );
+		} );
+
+		it( 'does not extract it from a valid locale without country', () => {
+			const r = currencyFromLocale( 'en' );
+			expect( r ).toEqual( null );
+		} );
+
+		it( 'does not extract it from a locale with an wrong country', () => {
+			const r = currencyFromLocale( 'en-WRONG' );
+			expect( r ).toEqual( null );
+		} );
+
+		it( 'extracts it from a locale with a correct country', () => {
+			const r = currencyFromLocale( 'en-US' );
+			expect( r ).toEqual( 'USD' );
 		} );
 
 	} );
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { currencyFromLocale } from "./locale";
+import countryToCurrency from 'country-to-currency';
 
 // @see https://tc39.es/ecma402/#sec-properties-of-intl-numberformat-instances
 
@@ -198,4 +198,61 @@ export function formatNumber(
 
     return new Intl.NumberFormat( locale, obj ).format( value );
 }
+
+/**
+ * Returns the currency code from a given locale with country code or `null` if not found.
+ * Whether the locale does not have a country code, it returns `null`.
+ *
+ * @example
+ *  - 'en-US' -> 'USD'
+ *  - 'de' -> null
+ *  - 'pt-BR' -> 'BRL'
+ *  - '?' -> null
+ *
+ * @param locale Locale
+ */
+export function currencyFromLocale( locale: string ): string | null {
+    const loc = extractCountryCode( locale );
+    return loc ? countryToCurrency[ loc ] || null : null;
+}
+
+/**
+ * Returns the country code from a locale or `null` if not defined.
+ *
+ * @example
+ *  - 'en-US'       -> 'US'
+ *  - 'zh-Hans-CN'  -> 'CN'
+ *  - 'en'          -> null
+ *  - '?'           -> null
+ *
+ * @param locale Locale
+ */
+export function extractCountryCode( locale: string ): string | null {
+    const [ , l2, l3 ] = ( locale || '' ).replace( '_', '-' ).split( '-' );
+    if ( l3 ) {
+        return l3.toUpperCase();
+    }
+    return l2 ? l2.toUpperCase() : null;
+}
+
+// /**
+//  * Format a locale.
+//  *
+//  * @example
+//  *  - 'en-us' -> 'en-US'
+//  *
+//  * @param locale Locale to format
+//  */
+// export function formatLocale( locale: string ): string {
+//     const [ l1, l2, l3 ] = locale.replace( '_', '-' ).split( '-' );
+//     const pieces = [];
+//     pieces.push( l1.toLowerCase() );
+//     if ( l2 && l3 ) {
+//         pieces.push( l2.charAt( 1 ).toUpperCase() + l2.substr( 1 ).toLowerCase() );
+//         pieces.push( l3.toUpperCase() );
+//     } else if ( l2 ) {
+//         pieces.push( l2.toUpperCase() );
+//     }
+//     return pieces.join( '-' );
+// }
 
